5b6de7970c4c5a4e6a8a1eef3da4c0bb918b6729
vishwa <11762008+00000vish@users.noreply.github.com>
2024-08-28 22:31:47 -0500

    dont resize past screensize

--- src/models/resizer.js
+++ src/models/resizer.js
@@ -1,6 +1,7 @@
 import GObject from 'gi://GObject';
 import Settings from '../helpers/settings.js';
 import * as windowHelper from '../helpers/window.js'
+import * as screenHelper from '../helpers/screen.js'
 
 export default GObject.registerClass(
     class Resizer extends GObject.Object {
@@ -31,16 +32,33 @@ export default GObject.registerClass(
                 return;
             }
 
-            let size = windowHelper.getWindowSize(window);
+            let workspace = windowHelper.getWorkspace(window);
+            let screenSize = screenHelper.getScreenSize(workspace);
+
+            let windowSize = windowHelper.getWindowSize(window);
             let amount = Settings.getResizeAmount() * 10 * (grow ? 1 : -1);
 
             if (vertical) {
-                size.height += amount;
+                windowSize.height += amount;
             } else {
-                size.width += amount;
+                windowSize.width += amount;
             }
 
-            windowHelper.resizeWindow(window, size);
+            let windowWidth = windowSize.x + windowSize.width;
+            let screenWidth = screenSize.x + screenSize.width;
+
+            let windowHeight = windowSize.y + windowSize.height;
+            let screenHeight = screenSize.y + screenSize.height;
+
+            if (windowWidth > screenWidth) {
+                windowSize.width -= windowWidth - screenWidth;
+            }
+
+            if (windowHeight > screenHeight) {
+                windowSize.height -= windowHeight - screenHeight;
+            }
+
+            windowHelper.resizeWindow(window, windowSize);
         }
 
         destroy() { }
