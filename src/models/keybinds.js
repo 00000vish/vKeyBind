a963ac796863b033f5834536486d59d0d30e136b
vishwa <11762008+00000vish@users.noreply.github.com>
2024-08-28 22:36:49 -0500

    clean up keybinds

--- src/models/keybinds.js
+++ src/models/keybinds.js
@@ -9,41 +9,10 @@ export default GObject.registerClass(
     class KeyBinds extends GObject.Object {
         _settings;
 
-        _tileCallback;
-
-        _switchCallbackRight;
-        _switchCallbackLeft;
-        _switchCallbackUp;
-        _switchCallbackDown;
-
-        _focusCallbackRight;
-        _focusCallbackLeft;
-        _focusCallbackUp;
-        _focusCallbackDown;
-
-        _growCallbackX;
-        _growCallbackY;
-        _shrinkCallbackX;
-        _shrinkCallbackY;
-
         constructor() {
             super();
 
             this._settings = Settings.reference;
-
-            this._setupKeyBinds(Settings.KEY_TILE, () => this._tileCallback?.());
-            this._setupKeyBinds(Settings.KEY_SWITCH_RIGHT, () => this._switchCallbackRight?.());
-            this._setupKeyBinds(Settings.KEY_SWITCH_LEFT, () => this._switchCallbackLeft?.());
-            this._setupKeyBinds(Settings.KEY_SWITCH_UP, () => this._switchCallbackUp?.());
-            this._setupKeyBinds(Settings.KEY_SWITCH_DOWN, () => this._switchCallbackDown?.());
-            this._setupKeyBinds(Settings.KEY_GROW_X, () => this._growCallbackX?.());
-            this._setupKeyBinds(Settings.KEY_GROW_Y, () => this._growCallbackY?.());
-            this._setupKeyBinds(Settings.KEY_SHRINK_X, () => this._shrinkCallbackX?.());
-            this._setupKeyBinds(Settings.KEY_SHRINK_Y, () => this._shrinkCallbackY?.());
-            this._setupKeyBinds(Settings.KEY_FOCUS_RIGHT, () => this._focusCallbackRight?.());
-            this._setupKeyBinds(Settings.KEY_FOCUS_LEFT, () => this._focusCallbackLeft?.());
-            this._setupKeyBinds(Settings.KEY_FOCUS_UP, () => this._focusCallbackUp?.());
-            this._setupKeyBinds(Settings.KEY_FOCUS_DOWN, () => this._focusCallbackDown?.());
         }
 
         _setupKeyBinds(settingKey, callback) {
@@ -59,19 +28,19 @@ export default GObject.registerClass(
 
         registerKeybind(settingKey, callback) {
             switch (settingKey) {
-                case Settings.KEY_TILE: this._tileCallback = callback; break;
-                case Settings.KEY_SWITCH_RIGHT: this._switchCallbackRight = callback; break;
-                case Settings.KEY_SWITCH_LEFT: this._switchCallbackLeft = callback; break;
-                case Settings.KEY_SWITCH_UP: this._switchCallbackUp = callback; break;
-                case Settings.KEY_SWITCH_DOWN: this._switchCallbackDown = callback; break;
-                case Settings.KEY_GROW_X: this._growCallbackX = callback; break;
-                case Settings.KEY_GROW_Y: this._growCallbackY = callback; break;
-                case Settings.KEY_SHRINK_X: this._shrinkCallbackX = callback; break;
-                case Settings.KEY_SHRINK_Y: this._shrinkCallbackY = callback; break;
-                case Settings.KEY_FOCUS_RIGHT: this._focusCallbackRight = callback; break;
-                case Settings.KEY_FOCUS_LEFT: this._focusCallbackLeft = callback; break;
-                case Settings.KEY_FOCUS_UP: this._focusCallbackUp = callback; break;
-                case Settings.KEY_FOCUS_DOWN: this._focusCallbackDown = callback; break;
+                case Settings.KEY_TILE: this._setupKeyBinds(Settings.KEY_TILE, callback); break;
+                case Settings.KEY_SWITCH_RIGHT: this._setupKeyBinds(Settings.KEY_SWITCH_RIGHT, callback); break;
+                case Settings.KEY_SWITCH_LEFT: this._setupKeyBinds(Settings.KEY_SWITCH_LEFT, callback); break;
+                case Settings.KEY_SWITCH_UP: this._setupKeyBinds(Settings.KEY_SWITCH_UP, callback); break;
+                case Settings.KEY_SWITCH_DOWN: this._setupKeyBinds(Settings.KEY_SWITCH_DOWN, callback); break;
+                case Settings.KEY_GROW_X: this._setupKeyBinds(Settings.KEY_GROW_X, callback); break;
+                case Settings.KEY_GROW_Y: this._setupKeyBinds(Settings.KEY_GROW_Y, callback); break;
+                case Settings.KEY_SHRINK_X: this._setupKeyBinds(Settings.KEY_SHRINK_X, callback); break;
+                case Settings.KEY_SHRINK_Y: this._setupKeyBinds(Settings.KEY_SHRINK_Y, callback); break;
+                case Settings.KEY_FOCUS_RIGHT: this._setupKeyBinds(Settings.KEY_FOCUS_RIGHT, callback); break;
+                case Settings.KEY_FOCUS_LEFT: this._setupKeyBinds(Settings.KEY_FOCUS_LEFT, callback); break;
+                case Settings.KEY_FOCUS_UP: this._setupKeyBinds(Settings.KEY_FOCUS_UP, callback); break;
+                case Settings.KEY_FOCUS_DOWN: this._setupKeyBinds(Settings.KEY_FOCUS_DOWN, callback); break;
                 default: return;
             }
         }
