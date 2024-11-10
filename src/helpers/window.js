9def57afa280ffeb76e77ce5cff5cf6fe4c310e7
vishwa <11762008+00000vish@users.noreply.github.com>
2024-11-10 00:35:58 -0600

    oops....

--- src/helpers/window.js
+++ src/helpers/window.js
@@ -104,13 +104,13 @@ function filterWindowDirection(focusWindow, windows, direction) {
     let filterCallback = (otherWindow) => {
         switch (direction) {
             case Direction.Up:
-                return otherWindow.size.y < focusWindow.size.y;
+                return otherWindow.size.y <= focusWindow.size.y;
             case Direction.Down:
-                return otherWindow.size.y > focusWindow.size.y + focusWindow.size.height;
+                return otherWindow.size.y >= focusWindow.size.y + focusWindow.size.height;
             case Direction.Left:
-                return otherWindow.size.x < focusWindow.size.x;
+                return otherWindow.size.x <= focusWindow.size.x;
             case Direction.Right:
-                return otherWindow.size.x > focusWindow.size.x + focusWindow.size.width;
+                return otherWindow.size.x >= focusWindow.size.x + focusWindow.size.width;
         }
         return false;
     }
