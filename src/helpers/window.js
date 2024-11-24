1bfb46349f5112a1918f079067b472e1bf29e376
vishwa <11762008+00000vish@users.noreply.github.com>
2024-11-23 18:44:59 -0600

    fix strange addition bug

--- src/helpers/window.js
+++ src/helpers/window.js
@@ -7,7 +7,7 @@ export function getFocusedWindow() {
 
     return {
         ref: window,
-        size: window.get_frame_rect()
+        size: window.get_frame_rect(),
     };
 }
 
@@ -25,12 +25,11 @@ export function getWindowsInWorkspace(workspace, activeMonitorOnly) {
     for (let window of windows) {
         let monitor = window.get_monitor();
 
-        if (monitor !== currentMonitor && activeMonitorOnly)
-            continue;
+        if (monitor !== currentMonitor && activeMonitorOnly) continue;
 
         windowList.push({
             ref: window,
-            size: window.get_frame_rect()
+            size: window.get_frame_rect(),
         });
     }
 
@@ -42,35 +41,28 @@ export function invokeOnWinowReady(window, callback) {
     windowActor.remove_all_transitions();
 
     let signal = windowActor.connect(
-        "first-frame",
+        'first-frame',
         ((_) => {
             callback(window);
 
             windowActor.disconnect(signal);
-
         }).bind(this)
     );
 }
 
 export function centerWindow(window, size) {
     let middle = {
-        x: (size.x + size.width / 2) - (window.size.width / 2),
-        y: (size.y + size.height / 2) - (window.size.height / 2),
+        x: size.x + size.width / 2 - window.size.width / 2,
+        y: size.y + size.height / 2 - window.size.height / 2,
     };
 
     window.ref.move_frame(false, middle.x, middle.y);
 }
 
 export function resizeWindow(window, size) {
-    window.ref.unmaximize(Meta.MaximizeFlags.BOTH)
+    window.ref.unmaximize(Meta.MaximizeFlags.BOTH);
     window.ref.move_frame(false, size.x, size.y);
-    window.ref.move_resize_frame(
-        false,
-        size.x,
-        size.y,
-        size.width,
-        size.height,
-    );
+    window.ref.move_resize_frame(false, size.x, size.y, size.width, size.height);
 }
 
 export function maximizeWindow(window) {
@@ -95,7 +87,7 @@ export function getNearbyWindows(window, direction, strict = true) {
 function getNearbyWindowsInner(window, activeMonitorOnly, direction) {
     let workspace = getWorkspace(window);
     let windows = getWindowsInWorkspace(workspace, activeMonitorOnly);
-    windows = windows.filter(x => x.ref.get_id() !== window.ref.get_id());
+    windows = windows.filter((x) => x.ref.get_id() !== window.ref.get_id());
     windows = filterWindowDirection(window, windows, direction);
     return sortWindows(window, windows, direction);
 }
@@ -106,30 +98,30 @@ function filterWindowDirection(focusWindow, windows, direction) {
             case Direction.Up:
                 return otherWindow.size.y <= focusWindow.size.y;
             case Direction.Down:
-                return otherWindow.size.y >= focusWindow.size.y + focusWindow.size.height;
+                return otherWindow.size.y >= (focusWindow.size.y + focusWindow.size.height) * 1;
             case Direction.Left:
                 return otherWindow.size.x <= focusWindow.size.x;
             case Direction.Right:
-                return otherWindow.size.x >= focusWindow.size.x + focusWindow.size.width;
+                return otherWindow.size.x >= (focusWindow.size.x + focusWindow.size.width) * 1;
         }
         return false;
-    }
+    };
 
     return windows.filter(filterCallback);
 }
 
 function getWindowEdgePoint(window, direction) {
     function getMiddle(start, size) {
-        return (start + (size / 2));
+        return start + size / 2;
     }
 
     switch (direction) {
         case Direction.Down:
             return [getMiddle(window.size.x, window.size.width), window.size.y];
         case Direction.Up:
-            return [getMiddle(window.size.x, window.size.width), (window.size.y + window.size.height)];
+            return [getMiddle(window.size.x, window.size.width), window.size.y + window.size.height];
         case Direction.Left:
-            return [(window.size.x + window.size.width), getMiddle(window.size.y, window.size.height)];
+            return [window.size.x + window.size.width, getMiddle(window.size.y, window.size.height)];
         case Direction.Right:
             return [window.size.x, getMiddle(window.size.y, window.size.height)];
     }
@@ -143,18 +135,17 @@ function sortWindows(focusWindow, windows, direction) {
     let [focusX, focusY] = getWindowEdgePoint(focusWindow, Direction.getOpposite(direction));
 
     for (let otherWindow of windows) {
-
         let [otherX, otherY] = getWindowEdgePoint(otherWindow, direction);
 
-        let valueOne = Math.pow((otherX - focusX), 2);
-        let valueTwo = Math.pow((otherY - focusY), 2);
-        let distance = Math.sqrt((valueOne + valueTwo));
+        let valueOne = Math.pow(otherX - focusX, 2);
+        let valueTwo = Math.pow(otherY - focusY, 2);
+        let distance = Math.sqrt(valueOne + valueTwo);
 
         calculatedWindows.push({
             window: otherWindow,
-            distance: distance
+            distance: distance,
         });
     }
 
-    return calculatedWindows.sort((a, b) => a.distance - b.distance).map(x => x.window);
+    return calculatedWindows.sort((a, b) => a.distance - b.distance).map((x) => x.window);
 }
